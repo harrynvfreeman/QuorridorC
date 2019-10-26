10f31823f9d6f30a8dd33f9668c4d95e051f2328
harrynvfreeman <harrynvfreeman@gmail.com>
2019-10-26 14:11:33 -0700

    stop_gradient labels

--- loss.py
+++ loss.py
@@ -11,6 +11,6 @@ def softmax_cross_entropy_with_logits(y_true, y_pred):
     negatives = tf.fill(tf.shape(pi), -100.0) 
     p = tf.where(where, negatives, p)
 
-    loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels = pi, logits = p)
+    loss = tf.nn.softmax_cross_entropy_with_logits_v2(labels = tf.stop_gradient(pi), logits = p)
 
     return loss
